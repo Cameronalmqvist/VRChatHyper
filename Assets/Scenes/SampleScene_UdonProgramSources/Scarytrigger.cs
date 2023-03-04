a46881378a9238b4d5d6a76ea3565209b9ed94e7
cameronalmqvist-admin <cameron.almqvist@hyperisland.se>
2023-03-04 13:36:40 +0100

    jobs done

---
+++ Assets/Scenes/SampleScene_UdonProgramSources/Scarytrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UdonSharp;
+using VRC.SDKBase;
+
+public class Scarytrigger : UdonSharpBehaviour
+{
+    public AudioSource soundSource;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        VRCPlayerApi player = other.GetComponent<VRCPlayerApi>();
+
+        if (player != null && player.isLocal)
+        {
+            soundSource.Play();
+        }
+    }
+}
